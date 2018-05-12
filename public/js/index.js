d271f497cb282981681b1a48d945397cfc848a3a
Dan Evron <danevron@gmail.com>
2018-05-12 15:33:21 +0300

    Adds Mustache

--- public/js/index.js
+++ public/js/index.js
@@ -12,22 +12,27 @@ socket.on('newMessage', function (message) {
   console.log('New message', message);
   var formattedTime = moment(message.createdAt).format('h:mm a');
 
-  var li = jQuery('<li></li>');
-  li.text(`${message.from} ${formattedTime}: ${message.text}`);
-  jQuery('#messages').append(li);
+  var template = jQuery('#message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    createdAt: formattedTime,
+    text: message.text
+  });
+
+  jQuery('#messages').append(html);
 });
 
 socket.on('newLocationMessage', function (message) {
-
   var formattedTime = moment(message.createdAt).format('h:mm a');
 
-  var li = jQuery('<li></li>');
-  var a = jQuery('<a target=_blank>My location</a>');
-  a.attr('href', message.url);
-  li.text(`${message.from} ${formattedTime}: `);
-  li.append(a);
+  var template = jQuery('#location-message-template').html();
+  var html = Mustache.render(template, {
+    from: message.from,
+    createdAt: formattedTime,
+    url: message.url
+  });
 
-  jQuery('#messages').append(li);
+  jQuery('#messages').append(html);
 });
 
 jQuery('#message-form').on('submit', function (e) {
